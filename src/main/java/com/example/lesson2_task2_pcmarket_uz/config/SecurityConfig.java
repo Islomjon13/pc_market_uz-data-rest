b84e9380424f44dfea30e178a0d742f8ea4df261
AnvarjonovIslomjon <islomjonanvarjonov@gmail.com>
2022-04-05 15:23:27 +0500

    add security

---
+++ src/main/java/com/example/lesson2_task2_pcmarket_uz/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.lesson2_task2_pcmarket_uz.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("superAdmin").password(passwordEncoder().encode("superAdmin")).roles("SUPER_ADMIN")
+                .and()
+                .withUser("moderator").password(passwordEncoder().encode("moderator")).roles("MODERATOR")
+                .and()
+                .withUser("operator").password(passwordEncoder().encode("operator")).roles("OPERATOR");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.DELETE,"/api/**").hasAnyRole("SUPER_ADMIN"  )
+                .antMatchers(HttpMethod.GET,"/api/**").hasAnyRole("SUPER_ADMIN","MODERATOR","OPERATOR")
+                .antMatchers("/api/**").hasAnyRole("SUPER_ADMIN","MODERATOR")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
